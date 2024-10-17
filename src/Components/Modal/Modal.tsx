48b5c5ae7992eb4ae8871c99a1037d9a7c7992ac
HydroponKing <momomolchenel@gmail.com>
2024-10-17 21:54:59 +0500

    update

---
+++ src/Components/Modal/Modal.tsx
@@ -0,0 +1,15 @@
+import style from "./Modal.module.css"
+
+export default function Modal ({active, setActive}){
+
+  return (
+    <div className={style.modal}>
+        <div className={style.modal__content}>
+
+        </div>
+
+    </div>
+  );
+}
+
+
